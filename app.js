2dbfd2000e3b66bd6a6db404dba26b014f4edbb5
Xiaotian Yu <johnsonyuri@hotmail.com>
2017-02-28 01:45:28 -0500

    implement signup and login

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
-var logger = require('morgan');
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
+mongoose.connect('mongodb://localhost/symposium');
+var db = mongoose.connection;
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,33 +31,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs('index'));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressValidator());
 
-app.use('/', index);
-app.use('/users', users);
+app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(passport.initialize());
+app.use(passport.session());
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
+
+
 
 // Express Validator
 app.use(expressValidator({
@@ -70,19 +69,50 @@ app.use(expressValidator({
   }
 }));
 
-
 // Connect to flash
 app.use(flash());
 
+
 // Global variables
 app.use(function (req, res, next) {
-  console.log("success");
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   res.locals.user = req.user || null;
   next();
 });
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+
+
+
+
+
+
 
 
 module.exports = app;
