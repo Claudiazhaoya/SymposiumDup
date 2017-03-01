e30c777ae007fad4c98d8855383cf9d19a4872c6
shirley <fu157@purdue.edu>
2017-03-01 04:07:53 -0500

    purdue api in the model/user

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		res.redirect('/users/login');
-	}
-}
-
 module.exports = router;
