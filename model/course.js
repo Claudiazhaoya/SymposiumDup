0dc9132ac0c15663f96271e1785b6542ee5011e5
kj cha <kj cha>
2017-03-01 00:53:53 -0500

    implement search (inc) added error page

---
+++ model/course.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var textSearch = require('mongoose-text-search')
+
+// Course Schema
+var CourseSchema = mongoose.Schema({
+  coursename: {
+    type: String
+  },
+  description: {
+    type: String
+  }
+});
+
+var Course = module.exports = mongoose.model('Course', CourseSchema);
+
+module.exports.searchCourse = function(coursename, callback) {
+  var query = {coursename: coursename};
+  Course.findOne(query, callback);
+};
