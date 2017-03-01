4c300548a0ef09264bfb0b4b1bb0d6b04ea0dba2
JINGZHOU WANG <wang2542@purdue.edu>
2017-03-01 02:38:45 -0500

    still have some bugs with discussion page and study group page

--- model/course.js
+++ model/course.js
@@ -1,31 +1,19 @@
 var mongoose = require('mongoose');
-<<<<<<< HEAD
-
-var CourseSchema = mongoose.Schema({
-   courseId: {
-     type: String
-   },
-   
-
-   
-});
-=======
 var textSearch = require('mongoose-text-search')
 
 // Course Schema
 var CourseSchema = mongoose.Schema({
-  coursename: {
-    type: String
-  },
-  description: {
-    type: String
-  }
+    coursename: {
+        type: String
+    },
+    description: {
+        type: String
+    }
 });
 
 var Course = module.exports = mongoose.model('Course', CourseSchema);
 
 module.exports.searchCourse = function(coursename, callback) {
-  var query = {coursename: coursename};
-  Course.findOne(query, callback);
+    var query = {coursename: coursename};
+    Course.findOne(query, callback);
 };
->>>>>>> 8bd052bc5041d5ffa2245f9a8e7b3f0f6e83c0e7
