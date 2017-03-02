5fc660692e59e3e629250e45de04b87ac3ec543d
kj <kj.j.cha@gmail.com>
2017-03-01 21:22:59 -0500

    update

---
+++ model/post.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+
+var ratingSchema = mongoose.Schema({
+	post_id:{
+	  type:String
+	}
+	user_id:{
+	  type:String
+	}
+	countUp: Number
+});
+var postSchema = mongoose.Schema({
+	infomation: {
+	  type: String,
+	  index:true
+	},
+	
+	course_id: {
+	  type: String
+	},
+	user_id:{
+	  type: String
+	}
+	timeStamp: {
+	  type: Date,
+	  default: Date.now
+        }
+	rating: [ratingSchema]
+	main_post_id: {
+	  type: String
+	}
+ }
+
+var post = module.exports = mongoose.model('post', postSchema);
+
+module.exports.createPost = function(newPost, callback) {
+    newUser.save(callback);
+}
+
+module.exports.getPostByCourseId = function(course_id, callback) {
+    var query = {course_id : course_id};
+    User.findone(query, callback);
+}
+
+module.exports.getPostByMainPost = function(post_id, callback) {
+    var query = {main_post_id : post_id};
+    user.findone(query,callback);
+}
