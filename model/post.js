4037b335e3d7cdcbc5b384838f1258813553760e
shirley <fu157@purdue.edu>
2017-04-05 21:58:51 -0400

    merge with shirley's post

--- model/post.js
+++ model/post.js
@@ -1,14 +1,5 @@
 var mongoose = require('mongoose');
 
-var ratingSchema = mongoose.Schema({
-	post_id:{
-	  type:String
-	},
-	user_id:{
-	  type:String
-	},
-	countUp: Number
-});
 var postSchema = mongoose.Schema({
 	information: {
 	  type: String,
@@ -25,7 +16,13 @@ var postSchema = mongoose.Schema({
 	  type: Date,
 	  default: Date.now
         },
-	rating: [ratingSchema],
+
+	countUp : {
+	  type: Number
+	},
+	countDown : {
+	  type: Number
+	},
 	main_post_id: {
 	  type: String
 	}
@@ -40,14 +37,14 @@ module.exports.createPost = function(newPost, callback) {
 
 module.exports.getPostByCourseId = function(course_id, callback) {
     console.log('get post by course id');
-    var query = {course_id : course_id, main_post_id : null };
-    post.find(query,callback);
+    var query = {course_id : course_id, main_post_id : '1' };
+    post.find({$query:query, $orderby:{countUp:1}},callback);
     
 }
 
 module.exports.getPostByMainPost = function(post_id, callback) {
     var query = {main_post_id : post_id};
-    post.find(query,callback);
+    post.find(query ,callback);
 }
 
 module.exports.deletePostById = function(post_id, callback) {
@@ -58,4 +55,14 @@ module.exports.deletePostById = function(post_id, callback) {
     });
 }
 
+module.exports.updataRatingById = function(post_id, rating, callback) {
+    var query = {_id : post_id};
+    var temp;
+    if(rating == 1) temp = "countUp";
+    else temp = "countDown";
+    post.findOneAndUpdate({
+       query,
+       $inc: {temp : 1}},callback
+    );
+}
 //module.exports.updataPostById = function(
