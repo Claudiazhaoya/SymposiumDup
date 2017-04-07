a6882b8f2e960e21093b5faa885657fc10460411
JINGZHOU WANG <wang2542@purdue.edu>
2017-04-07 18:03:31 -0400

    update

--- model/post.js
+++ model/post.js
@@ -1,32 +1,32 @@
 var mongoose = require('mongoose');
 
 var postSchema = mongoose.Schema({
-    information: {
-        type: String,
-        index:true
-    },
+	information: {
+	  type: String,
+	  index:true
+	},
+	
+	course_id: {
+	  type: String
+	},
+	user_id:{
+	  type: String
+	},
+	timeStamp: {
+	  type: Date,
+	  default: Date.now
+        },
 
-    course_id: {
-        type: String
-    },
-    user_id:{
-        type: String
-    },
-    timeStamp: {
-        type: Date,
-        default: Date.now
-    },
-
-    countUp : {
-        type: Number
-    },
-    countDown : {
-        type: Number
-    },
-    main_post_id: {
-        type: String
-    }
-});
+	countUp : {
+	  type: Number
+	},
+	countDown : {
+	  type: Number
+	},
+	main_post_id: {
+	  type: String
+	}
+ });
 
 var post = module.exports = mongoose.model('post', postSchema);
 module.exports.createPost = function(newPost, callback) {
@@ -39,7 +39,7 @@ module.exports.getPostByCourseId = function(course_id, callback) {
     console.log('get post by course id');
     var query = {course_id : course_id, main_post_id : '1' };
     post.find({$query:query, $orderby:{countUp:1}},callback);
-
+    
 }
 
 module.exports.getPostByMainPost = function(post_id, callback) {
@@ -50,8 +50,8 @@ module.exports.getPostByMainPost = function(post_id, callback) {
 module.exports.deletePostById = function(post_id, callback) {
     var query = {_id : post_id};
     post.findOneAndRemove(query,function(err, post) {
-        if(err) throw err;
-        console.log('post: '+ post_id + 'is deleted!');
+       if(err) throw err;	
+	console.log('post: '+ post_id + 'is deleted!');
     });
 }
 
@@ -61,7 +61,8 @@ module.exports.updataRatingById = function(post_id, rating, callback) {
     if(rating == 1) temp = "countUp";
     else temp = "countDown";
     post.findOneAndUpdate({
-        query,
-        $inc: {temp : 1}},callback
+       query,
+       $inc: {temp : 1}},callback
     );
 }
+//module.exports.updataPostById = function(
