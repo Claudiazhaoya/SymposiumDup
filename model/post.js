b65b4cc41ed12a7c18605d3652d750b94ce69994
shirley <fu157@purdue.edu>
2017-04-25 16:59:03 -0400

    rating process

--- model/post.js
+++ model/post.js
@@ -14,7 +14,10 @@ var postSchema = mongoose.Schema({
 	date : {
 	  type: String
         },
-
+        timestamp : {
+	  type: Date,
+          default: Date.now
+	},
 	countUp : {
 	  type: Number
 	},
@@ -32,11 +35,16 @@ module.exports.createPost = function(newPost, callback) {
     newPost.save(callback);
 }
 
+module.exports.getMostPopuPost = function(course_id, callback) {
+    console.log('get most popular post');
+    var query = {course_id : course_id , main_post_is : '1'};
+    post.find(query).sort({courtUp: -1, timestamp: -1}).limit(1).exec(callback);
+}
 
 module.exports.getPostByCourseId = function(course_id, callback) {
     console.log('get post by course id');
     var query = {course_id : course_id, main_post_id : '1' };
-    post.find(query).sort({countUp:-1}).exec(callback);
+    post.find(query).sort({timestamp:-1}).exec(callback);
     
 }
 
